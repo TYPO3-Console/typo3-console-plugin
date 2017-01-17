6b964fb572692c2ada04979aeb2a2faf16ca60c0
Helmut Hummel <info@helhum.io>
2017-01-17 10:36:15 +0100

    [BUGFIX] Do not interfere with TYPO3 plugin
    In order to be in line with the TYPO3 Composer plugin
    we must adapt our paths accordingly.

--- res/php/autoload-include.tmpl.php
+++ res/php/autoload-include.tmpl.php
@@ -1,9 +1,12 @@
 <?php
-if (!getenv('TYPO3_PATH_WEB')) {
-    putenv('TYPO3_PATH_WEB=' . '{$web-dir}');
+if (!getenv('TYPO3_PATH_COMPOSER_ROOT')) {
+    putenv('TYPO3_PATH_COMPOSER_ROOT=' . '{$root-dir}');
 }
 if (!getenv('TYPO3_PATH_ROOT')) {
-    putenv('TYPO3_PATH_ROOT=' . '{$root-dir}');
+    putenv('TYPO3_PATH_ROOT=' . '{$web-dir}');
+}
+if (!getenv('TYPO3_PATH_WEB')) {
+    putenv('TYPO3_PATH_WEB=' . '{$web-dir}');
 }
 if (!getenv('TYPO3_ACTIVE_FRAMEWORK_EXTENSIONS')) {
     putenv('TYPO3_ACTIVE_FRAMEWORK_EXTENSIONS=' . '{$active-typo3-extensions}');
