9d79c0a271f1d6a4d51c74936b9325ad1a1a698e
Helmut Hummel <info@helhum.io>
2017-01-26 12:55:03 +0100

    [TASK] Add warning messages for some configuration
    If TYPO3 composer installers are improperly configured,
    TYPO3 Console might not work correctly, or the setup
    might be insecure.
    
    Issue a warning for these cases

--- src/IncludeFile/WebDirToken.php
+++ src/IncludeFile/WebDirToken.php
@@ -66,11 +66,31 @@ class WebDirToken implements TokenInterface
      */
     public function getContent()
     {
+        $this->validateComposerConfiguration();
         $includeFileFolder = dirname(dirname(__DIR__)) . '/res/php';
         return $this->filesystem->findShortestPathCode(
             $includeFileFolder,
             $this->typo3PluginConfig->get('web-dir'),
             true
         );
+    }
+
+    /**
+     * Validates the TYPO3 composer installer config and issues warning messages if appropriate
+     */
+    private function validateComposerConfiguration()
+    {
+        if ($this->typo3PluginConfig->get('prepare-web-dir') === false && $this->typo3PluginConfig->get('extensions-in-vendor-dir') === true) {
+            $this->io->writeError(chr(10) . '<warning>TYPO3 plugin installer configuration "prepare-web-dir" was set to false and "extensions-in-vendor-dir" was set to true</warning>');
+            $this->io->writeError(sprintf('<warning>TYPO3 Console will nevertheless assume "%s" to be the TYPO3 root directory.</warning>', $this->typo3PluginConfig->get('web-dir', Typo3PluginConfig::RELATIVE_PATHS)));
+            $this->io->writeError('<warning>In case you chose to setup TYPO3 in a different directory, TYPO3 Console will not work.</warning>' . chr(10));
+        }
+        $normalizedWebDir = $this->filesystem->normalizePath($this->typo3PluginConfig->get('web-dir'));
+        $normalizedBaseDir = $this->filesystem->normalizePath($this->typo3PluginConfig->getBaseDir());
+        if ($normalizedWebDir === $normalizedBaseDir) {
+            $this->io->writeError(chr(10) . '<warning>TYPO3 is configured to be installed in the composer root directory.</warning>');
+            $this->io->writeError('<warning>Doing so is bad practice and can be a security risk.</warning>');
+            $this->io->writeError('<warning>Please consider using the setting "web-dir" to configure an explicit web directory for TYPO3.</warning>' . chr(10));
+        }
     }
 }
